b1949261d975080d9138aa188462f0a5b34a4b32
LuisGSandoval <luis.sandoval.457@outlook.com>
2018-06-10 15:23:02 -0400

    Se agrega manejo de errores, teniendo en cuenta que el usuario no usara metamask

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SmartContractService } from '../../services/smart-contract.service'
 
 declare var M:any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent implements OnInit {
 
-  constructor(private router : Router) { }
+export class NavbarComponent implements OnInit {
+  
+  constructor( private router : Router, private contractServices: SmartContractService,) {
+  }
+  account: any;
   showRegisterForm : boolean = false;
   userIsSet : boolean = false;
   userNickname : string = '';
   searching : boolean = false;
   
+
   ngOnInit() {
+    if(this.contractServices.metamaskHolder == false){
+      alert('Deberías probar metamask')
+    } 
+
+
+    //this.account = this.contractServices.getUserAccount();
+    // let accountAddress : any ;
+    //let getAccount = this.contractServices.getAccount();
+    // console.log(getAccount.toString())
+    // accountAddress = this.contractServices.getAccount().then(data => {
+    //   return data;
+    // });
+    // console.log(accountAddress[1].toString())
+
+    
+
+
+
+    ///******************************/
+    ///Inicializaciones para materialize
+
     let modalOptions = new Object();
     var modalElems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(modalElems, modalOptions);
@@ -26,8 +52,10 @@ export class NavbarComponent implements OnInit {
     var selectElemsOptions = new Object();
     var selectElems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(selectElems, selectElemsOptions);
+    ///******************************/
   }
 
+//  Mostrar formulario de registro
   getRegisterForm(){
     if(this.showRegisterForm == false){
       this.showRegisterForm = true;
@@ -35,33 +63,23 @@ export class NavbarComponent implements OnInit {
       this.showRegisterForm = false;
     }
   }
+// Utilizar el nombre del usuario para la vsita
+  setNickname(){
+    if(this.userNickname.length < 2){
+      M.toast({html: 'Please fill out the form!'}); 
+    }
+  }
 
-  showSearchInput(){
+// Mostrar input para realizar busqueda
+  showSearchInput() {
     if(this.searching == true){
       this.searching = false;
     }else{
-      this.searching = true;
-      document.getElementById('search-input').focus();
+      this.searching = true; 
     }
   }
-
-  setNickname(){
-    if(this.userNickname.length > 2){
-
-      if(this.searching == false){
-        this.searching = true;
-      }else{
-        this.searching = false;
-      }
-    }else{
-      M.toast({html: 'Please fill out the form!'})
-    }
-    
-  }
-
+// realizar busqueda de topic seleccionado
   onSubmitSearchImages(){
-    this.router.navigate(['/videoSearch'])
-    
+    this.router.navigate(['/videoSearch']); 
   }
-
 }
--- src/app/services/smart-contract.service.ts
+++ src/app/services/smart-contract.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Rx';
+// import { Subject } from 'rxjs/Rx';
 // import Web3 from 'web3';
 declare let require: any;
 declare let window: any;
@@ -10,16 +10,21 @@ export class SmartContractService {
 
   public tokenABI = require('./contract-one.json');
 
+
+  public metamaskHolder: boolean = false;
+
+
   private _tokenContract: any;
   private _tokenContractAddress = '0x3e7b4d4ff330797ae1fb448310c99bcf80f51601';
 
   private web3: any;
-  private accounts: string[];
-  public accountsObservable = new Subject<string[]>();
+  //private accounts: string[]; 
+  private _account: string = null;
+  // public accountsObservable = new Subject<string[]>();
 
 
   constructor() {
-      this.bootstrapWeb3();
+    this.bootstrapWeb3();
   }
 
 
@@ -28,27 +33,67 @@ export class SmartContractService {
     if (typeof window.web3 !== 'undefined') {
       // Use Mist/MetaMask's provider
       this.web3 = new Web3(window.web3.currentProvider);
+      this.metamaskHolder = true
     } else {
-      console.log('No web3? You should consider trying MetaMask!');
+      this.metamaskHolder = false;
+      // // // alert('No web3? You should consider trying MetaMask!');
 
-      // Hack to provide backwards compatibility for Truffle, which uses web3js 0.20.x
-      Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
-      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      this.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+      // // // Hack to provide backwards compatibility for Truffle, which uses web3js 0.20.x
+      // // Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
+      // // // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      // // this.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+    }
+    if (this.metamaskHolder == true) {
+      this._tokenContract = new this.web3.eth.Contract(this.tokenABI, this._tokenContractAddress);
     }
 
-    this._tokenContract = this.web3.eth.contract(this.tokenABI).at(this._tokenContractAddress);
-
   }
+
+  public async getAccount(): Promise<string> {
+    if (this.metamaskHolder == true) {
+
+
+      if (this._account == null) {
+        this._account = await new Promise((resolve, reject) => {
+          this.web3.eth.getAccounts((err, accs) => {
+            if (err != null) {
+              alert('There was an error fetching your accounts.');
+              return;
+            }
+
+            if (accs.length === 0) {
+              alert(
+                'Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.'
+              );
+              return;
+            }
+            resolve(accs[0]);
+          })
+        }) as string;
+        this.web3.eth.defaultAccount = this._account;
+      }
+      return Promise.resolve(this._account);
+    }
+  }
+
+
+  // public async getUserAccount(): Promise<string> {
+  //   this._account = await this.getAccount();
+  //   // console.log(_account)
+  //   return this._account;
+  // }
+
+  // getUsersAccountAddressFromMetamask(){
+  //   return this.web3.currentProvider.publicConfigStore;
+  // }
+
 
 
   printRandomInfo() {
-    console.log(this.web3 )
+    if (this.metamaskHolder == true) {
+    // console.log(this.web3.currentProvider.publicConfigStore._state)
+    // console.log(this.getUserAccount());
+    console.log(window.web3)
+    }
   }
-
-
-
-
-
-
 }
